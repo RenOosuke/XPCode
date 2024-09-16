4e082208e76842cf94d6fe5c3fea018d56db4358
RenOosuke <oosukeren@gmail.com>
2024-09-16 16:56:16 -0700

    Adding Ace.js as base editor

---
+++ SourceCode/src-noconflict/snippets/makefile.js
@@ -0,0 +1,17 @@
+ace.define("ace/snippets/makefile.snippets",[], function(require, exports, module){module.exports = "snippet ifeq\n\tifeq (${1:cond0},${2:cond1})\n\t\t${3:code}\n\tendif\n";
+
+});
+
+ace.define("ace/snippets/makefile",[], function(require, exports, module){"use strict";
+exports.snippetText = require("./makefile.snippets");
+exports.scope = "makefile";
+
+});
+                (function() {
+                    ace.require(["ace/snippets/makefile"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
---
+++ SourceCode/src-noconflict/snippets/razor.js
@@ -0,0 +1,17 @@
+ace.define("ace/snippets/razor.snippets",[], function(require, exports, module){module.exports = "snippet if\n(${1} == ${2}) {\n\t${3}\n}";
+
+});
+
+ace.define("ace/snippets/razor",[], function(require, exports, module){"use strict";
+exports.snippetText = require("./razor.snippets");
+exports.scope = "razor";
+
+});
+                (function() {
+                    ace.require(["ace/snippets/razor"], function(m) {
+                        if (typeof module == "object" && typeof exports == "object" && module) {
+                            module.exports = m;
+                        }
+                    });
+                })();
+            
