37821f27321679de051065d77f973ad4584ae4f0
RenOosuke <oosukeren@gmail.com>
2024-09-23 04:15:38 -0700

    clearing out devtools

--- SourceCode/public/devTools.js
+++ SourceCode/public/devTools.js
@@ -1,21 +1,3 @@
-// nwgui = window.require("nw.gui");
-// nwwin = nwgui.Window.get(window);
-// process.removeAllListeners("uncaughtException");
-// process.on("uncaughtException", function(e) {
-//   var ref;
-//   if (!window.CRASHED) {
-//     window.CRASHED = true;
-//     if (typeof console !== "undefined" && console !== null) {
-//       if (typeof console.warn === "function") {
-//         console.warn("CRASH");
-//       }
-//     }
-//     nwwin.showDevTools();
-//   }
-//   if (((ref = nwgui.App.manifest.window) != null ? ref.show : void 0) === false) {
-//     return nwwin.show();
-//   }
-// });
 window.addEventListener("keydown", function(e) {
     var ref;
     let nwWin = nw.Window.get(window);
