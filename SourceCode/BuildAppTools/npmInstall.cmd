5a387e6fc84d4786886b1d28e065b0b91287dc2e
RenOosuke <oosukeren@abv.bg>
2025-01-20 20:19:11 +0200

    missed file

--- SourceCode/BuildAppTools/npmInstall.cmd
+++ SourceCode/BuildAppTools/npmInstall.cmd
@@ -1,7 +1,14 @@
 @echo off
-set projectName=XPCode
+setlocal enabledelayedexpansion
 
-cd ../BuiltApp/%projectName%/package.nw
+:: Read APPLICATION_NAME from a text file (located in the parent directory)
+cd ..
+for /f "delims=" %%a in (appname.txt) do set APPLICATION_NAME=%%a
+cd /d "%~dp0"
+
+:: Navigate to the package directory
+cd ../BuiltApp/%APPLICATION_NAME%/package.nw
 echo %cd%
 
+:: Install dependencies
 call npm install
